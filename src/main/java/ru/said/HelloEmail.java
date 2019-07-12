930af827b88fdc6d7be666e3a2f68b2d883a06ba
SMYU <sm.yusupov@mail.ru>
2019-07-12 17:57:03 +0300

    work comp

---
+++ src/main/java/ru/said/HelloEmail.java
@@ -0,0 +1,13 @@
+package ru.said;
+
+import javax.mail.*;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
+import java.util.Properties;
+
+public class HelloEmail {
+
+     HelloEmail() {
+
+     }
+}
