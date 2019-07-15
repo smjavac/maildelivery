5ef05dacab93d5cc632a326dd123cc6c51c7ff92
SMYU <sm.yusupov@mail.ru>
2019-07-15 17:56:30 +0300

    work comp

--- src/main/java/ru/said/service/UserService.java
+++ src/main/java/ru/said/service/UserService.java
@@ -1,11 +1,15 @@
 package ru.said.service;
 
+import javax.mail.*;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.Properties;
 
 public class UserService {
     private UserService() {}
@@ -29,5 +33,40 @@ public class UserService {
             strBuilder.append(b);
         }
         return strBuilder.toString();
+    }
+
+    public static void sendMessge(String email, String messageTxt ) throws MessagingException {
+        Properties properties = new Properties();
+        properties.put("mail.transport.protocol", "smtp");
+        //Хост или IP-адрес почтового сервера
+        properties.put("mail.smtp.host", "smtp.yandex.ru");
+        //Требуется ли аутентификация для отправки сообщения
+        properties.put("mail.smtp.auth", "true");
+        //Порт для установки соединения
+        properties.put("mail.smtp.socketFactory.port", "465");
+        //Фабрика сокетов, так как при отправке сообщения Yandex требует SSL-соединения
+        properties.put("mail.smtp.socketFactory.class", "javax.net.ssl.SSLSocketFactory");
+        //Создаем соединение для отправки почтового сообщения
+        Session session = Session.getDefaultInstance(properties,
+                //Аутентификатор - объект, который передает логин и пароль
+                new Authenticator() {
+                    @Override
+                    protected PasswordAuthentication getPasswordAuthentication() {
+                        return new PasswordAuthentication("sm.yusupov@yandex.ru", "s07071983");
+                    }
+                });
+        //Создаем новое почтовое сообщение
+        Message message = new MimeMessage(session);
+        //От кого
+        message.setFrom(new InternetAddress("sm.yusupov@yandex.ru"));
+        //Кому
+        message.setRecipient(Message.RecipientType.TO, new InternetAddress(email));
+        //Тема письма
+        message.setSubject("Очень важное письмо!!!");
+        //Текст письма
+        message.setText(messageTxt);
+        //Поехали!!!
+        Transport.send(message);
+
     }
 }
